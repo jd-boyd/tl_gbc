42863e0e242685bde5bfd0268666c0da7dce3301
Joshua D. Boyd <jdboyd@jdboyd.net>
2023-08-20 02:35:24 -0400

    Complete initial sound example.

--- 03_sound/main.c
+++ 03_sound/main.c
@@ -22,6 +22,10 @@ void position_world(int x, int y) {
 
 void main(void)
 {
+    NR52_REG = 0x80; // is 1000 0000 in binary and turns on sound
+    NR50_REG = 0x77; // sets the volume for both left and right channel just set to max 0x77
+    NR51_REG = 0xFF; // is 1111 1111 in binary, select which chanels we want to use in this case all of them. One bit for the L one bit for the R of all four channels
+
 
     set_sprite_data(0, 8, HWTiles);
     set_sprite_tile(0, 1); //H
@@ -52,34 +56,41 @@ void main(void)
 
     SHOW_SPRITES;
 
+
+
+
 #define MAX_X 150
 #define MAX_Y 152
 
     // Loop forever
     while(1) {
 		// Game main loop processing goes here
-
+        int should_beep = 0;
         position_hello(h_x, h_y);
         position_world(w_x, w_y);
 
         if (h_x + h_dx >= MAX_X
             || h_x + h_dx < 0 ) {
             h_dx *= -1;
+            should_beep = 1;
         }
 
         if (h_y + h_dy >= MAX_Y
             || h_y + h_dy < 1 ) {
             h_dy *= -1;
+            should_beep = 1;
         }
 
         if (w_x + w_dx >= MAX_X
             || w_x + w_dx < 0 ) {
             w_dx *= -1;
+            should_beep = 1;
         }
 
         if (w_y + w_dy >= MAX_Y
             || w_y + w_dy < 1 ) {
             w_dy *= -1;
+            should_beep = 1;
         }
 
         h_x += h_dx;
@@ -87,6 +98,17 @@ void main(void)
 
         w_x += w_dx;
         w_y += w_dy;
+
+        if (should_beep) {
+            // Values taken from fidling with sound example program
+            NR10_REG = 0x69;
+            NR11_REG = 0x42;
+            NR12_REG = 0x53;
+            // Last two are frequency, LSB first
+            NR13_REG = 0x77;
+            NR14_REG = 0x86;
+
+        }
 
 		// Done processing, yield CPU and wait for start of next frame
         vsync();
